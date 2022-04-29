77237ffebe6cf4a4eb2da01e29dfdbef7440bd5b
Antonio Dambujo <pdambuja@gmail.com>
2022-04-29 21:18:30 +0200

    restructure the project

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "29c4754e83339551"; }
+/******/ 		__webpack_require__.h = function() { return "856819ffdfdaea20"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
