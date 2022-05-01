9ca15995e717e25fa5e0831dec95155f833fb74b
Antonio Dambujo <pdambuja@gmail.com>
2022-05-01 09:20:56 +0200

    fix bug

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3771a404503487fe"; }
+/******/ 		__webpack_require__.h = function() { return "bb44c9520483a07b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
